0474232033c4ab9750620cb473c15f4f7c4252f2
Jason Wall <javajo@gmail.com>
2015-05-11 16:52:09 -0600

    initializer for new bootstrap directory

--- lib/yardstick-assets.rb
+++ lib/yardstick-assets.rb
@@ -1,9 +1,10 @@
 require "yardstick-assets/version"
 require 'lesselements-rails'
-require 'less-rails-bootstrap'
 
 module YardstickAssets
   class Engine < ::Rails::Engine
-    # I think this enables the asset pipeline?
+    initializer 'less-rails-bootstrap.setup', :after => 'less-rails.after.load_config_initializers', :group => :all do |app|
+      app.config.less.paths << File.join(config.root, 'vendor', 'frameworks')
+    end
   end
 end
